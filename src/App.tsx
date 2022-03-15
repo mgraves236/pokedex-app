fbeaf3c6f3686f11dc4ebbb5e1c919f8c44afab0
mgraves236 <m.graves236@gmail.com>
2022-03-15 14:24:11 +0100

    started pagination number

--- src/App.tsx
+++ src/App.tsx
@@ -9,36 +9,56 @@ function App() {
     const [currentUrl, setCurrentUrl] = useState("https://pokeapi.co/api/v2/pokemon");
     const [nextUrl, setNextUrl] = useState("");
     const [previousUrl, setPreviousUrl] = useState("");
+    const [currentPageNumber, setCurrentPageNumber] = useState(1);
+    const pageLimit = 5; // how many page numbers are displayed for the user
+    let pagesNumber;
     // loading screen
     const [loading, setLoading] = useState(true); // by def site is loading
-    var pageNumber:number = 1;
-    useEffect(() => { // added for efficency
+    useEffect(() => { // added for efficiency
         setLoading(true);
         const controller = new AbortController();
         axios.get(currentUrl, {
             signal: controller.signal
         }).then(response => {
             setLoading(false);
+            // number of pages from the response
+            pagesNumber = Math.round(response.data.left / pageLimit);
+
             if (response.data.previous == null) {
                 setPreviousUrl(currentUrl);
             } else {
                 setPreviousUrl(response.data.previous);
+                setCurrentPageNumber((page) => page - 1);
             }
             if (response.data.next == null) {
                 setNextUrl(currentUrl);
             } else {
                 setNextUrl(response.data.next);
+                setCurrentPageNumber((page) => page + 1);
             }
             updatePokemon(response.data.results.map((p: any) => p.name));
+        }).catch(error => {
+            if (error.response) {
+                // made a request, received wrong status code
+                console.log(error.response.data);
+                console.log(error.response.status);
+                console.log(error.response.headers);
+            } else if (error.request) {
+                // request made no response
+                console.log(error.request);
+            } else {
+                console.log('Error', error.message);
+            }
+            console.log(error.config);
         })
 
         // cancel older request after making a new request
         return () => controller.abort();
-    }, [currentUrl]) // everytime arguments are changed in the array the effect is rerun -- empty array no rerun
+    }, [currentUrl]); // everytime arguments are changed in the array the effect is rerun -- empty array no rerun
 
     if (loading) {
-        let text:string =  "Loading...";
-        return(
+        let text: string = "Loading...";
+        return (
             <div>
                 {text}
             </div>
@@ -54,13 +74,29 @@ function App() {
         setCurrentUrl(previousUrl);
     }
 
+    function changePage(event: any) {
+        const pageNumber = Number(event.target.textContent); // convert to a number the item that the user clicked on
+        setCurrentPageNumber(pageNumber);
+        setCurrentUrl("https://pokeapi.co/api/v2/pokemon?offset=" + 20 * pageNumber + "&limit=20");
+        console.log(pageNumber);
+    }
+
+    const getPaginationGroup = () => {
+        let start = Math.floor((currentPageNumber - 1) / pageLimit) * pageLimit;
+        return new Array(pageLimit).fill(0).map((_, idx) => start + idx + 1);
+    };
+
     return (
         <>
-        <PokemonList pokemon={pokemon}/>
-        <Pagination
-            goToNextPage = {goToNextPage}
-            goToPreviousPage = {goToPreviousPage}
-        />
+            <PokemonList pokemon={pokemon}/>
+            <Pagination
+                goToNextPage={goToNextPage}
+                goToPreviousPage={goToPreviousPage}
+                getPaginationGroup={getPaginationGroup}
+                changePage={changePage}
+                currentPageNumber={currentPageNumber}
+                pagesNumber={pagesNumber}
+            />
         </>
     );
 }
--- src/Pagination.tsx
+++ src/Pagination.tsx
@@ -3,10 +3,30 @@ import React from 'react';
 export default function Pagination(props:any) {
     let goToNext = props.goToNextPage;
     let goToPrev = props.goToPreviousPage;
+    let getPaginationGroup = props.getPaginationGroup;
+    let changePage = props.changePage;
+    let currentPageNumber = props.currentPageNumber;
+    let pagesNumber = props.pagesNumber;
     return (
         <div>
-            <button onClick={goToPrev}>Previous</button>
-            <button onClick={goToNext}>Next</button>
+            <button
+                onClick={goToPrev}
+                className={`prev ${currentPageNumber === 1 ? 'disabled' : ''}`}
+            >Previous</button>
+            <button
+                onClick={goToNext}
+                className={`next ${currentPageNumber === pagesNumber ? 'disabled' : ''}`}
+            >
+            >Next</button>
+            {getPaginationGroup().map((item:any, index:any) => (
+                <button
+                    key={index}
+                    onClick={changePage}
+                    className={`paginationItem ${currentPageNumber === item ? 'active' : null}`}
+                >
+                    <span>{item}</span>
+                </button>
+            ))}
         </div>
     );
 }
