ab21569787c9b84da7aa442111dab35dddc0e20e
mgraves236 <m.graves236@gmail.com>
2022-03-15 02:40:55 +0100

    list added

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,68 @@
-import React, { useState } from 'react';
+import React, {useState, useEffect} from 'react';
 import PokemonList from "./PokemonList";
 import axios from 'axios';
+import Pagination from "./Pagination";
 
 function App() {
-  const [pokemon, updatePokemon] = useState([]); // no initial state
+    const [pokemon, updatePokemon] = useState([]); // no initial state
+    // state to track current page
+    const [currentUrl, setCurrentUrl] = useState("https://pokeapi.co/api/v2/pokemon");
+    const [nextUrl, setNextUrl] = useState("");
+    const [previousUrl, setPreviousUrl] = useState("");
+    // loading screen
+    const [loading, setLoading] = useState(true); // by def site is loading
+    var pageNumber:number = 1;
+    useEffect(() => { // added for efficency
+        setLoading(true);
+        const controller = new AbortController();
+        axios.get(currentUrl, {
+            signal: controller.signal
+        }).then(response => {
+            setLoading(false);
+            if (response.data.previous == null) {
+                setPreviousUrl(currentUrl);
+            } else {
+                setPreviousUrl(response.data.previous);
+            }
+            if (response.data.next == null) {
+                setNextUrl(currentUrl);
+            } else {
+                setNextUrl(response.data.next);
+            }
+            updatePokemon(response.data.results.map((p: any) => p.name));
+        })
 
-  axios.get('https://pokeapi.co/api/v2/pokemon').then(response => {
-    updatePokemon(response.data.results.map((p:any) => p.name));
-  })
-  return (
-    <PokemonList pokemon={pokemon}/>
-  );
+        // cancel older request after making a new request
+        return () => controller.abort();
+    }, [currentUrl]) // everytime arguments are changed in the array the effect is rerun -- empty array no rerun
+
+    if (loading) {
+        let text:string =  "Loading...";
+        return(
+            <div>
+                {text}
+            </div>
+        )
+    }
+
+    // pagination
+    function goToNextPage() {
+        setCurrentUrl(nextUrl);
+    }
+
+    function goToPreviousPage() {
+        setCurrentUrl(previousUrl);
+    }
+
+    return (
+        <>
+        <PokemonList pokemon={pokemon}/>
+        <Pagination
+            goToNextPage = {goToNextPage}
+            goToPreviousPage = {goToPreviousPage}
+        />
+        </>
+    );
 }
 
 export default App;
--- src/Pagination.tsx
+++ src/Pagination.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
-export default function Pagination() {
+export default function Pagination(props:any) {
+    let goToNext = props.goToNextPage;
+    let goToPrev = props.goToPreviousPage;
     return (
         <div>
-
+            <button onClick={goToPrev}>Previous</button>
+            <button onClick={goToNext}>Next</button>
         </div>
     );
 }
